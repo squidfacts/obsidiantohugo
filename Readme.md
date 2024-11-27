52113d91e7c43a8a307c7c245a574df7c81584e1
Emma Reuter <squid@emmareuter.com>
2024-11-27 16:46:46 -0600

    started working on hugo post generation

--- Readme.md
+++ Readme.md
@@ -4,13 +4,13 @@
 
 ```
 cargo run -- --obsidian-dir ~/Desktop/vault/Publish --obsidian-dir-imgs ~/Desktop/vault/Imgs/ --hugo-path ~/blag/
-   Compiling blogger v0.1.0 (/Users/exit/blogger)
-    Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.56s
+    Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.02s
      Running `target/debug/blogger --obsidian-dir /Users/exit/Desktop/vault/Publish --obsidian-dir-imgs /Users/exit/Desktop/vault/Imgs/ --hugo-path /Users/exit/blag/`
-static base /Users/exit/blag//static
-copied /Users/exit/Desktop/vault/Imgs/Pasted image 20241127141342.png to /Users/exit/blag/lifeos2.00.png
-copied /Users/exit/Desktop/vault/Imgs/Pasted image 20241127141604.png to /Users/exit/blag/lifeos2.01.png
-copied /Users/exit/Desktop/vault/Imgs/Pasted image 20241127143642.png to /Users/exit/blag/lifeos2.02.png
+created static folder /Users/exit/blag/static/lockbit2
+Creating hugo md file /Users/exit/blag/content/posts/lockbit2.md
+copied /Users/exit/Desktop/vault/Imgs/Pasted image 20241127141342.png to /Users/exit/blag/static/lockbit2/lockbit20.png
+copied /Users/exit/Desktop/vault/Imgs/Pasted image 20241127141604.png to /Users/exit/blag/static/lockbit2/lockbit21.png
+copied /Users/exit/Desktop/vault/Imgs/Pasted image 20241127143642.png to /Users/exit/blag/static/lockbit2/lockbit22.png
 ```
 
 ### Note
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use clap::Parser;
 use std::fs;
-
 use std::fs::read_to_string;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn process_blog(
     hugo_base_dir: &String,
@@ -14,27 +15,38 @@ fn process_blog(
     let mut count = 0;
 
     let mut parsed_path = String::new();
-    let mut image_base = String::new();
 
+    let mut file: Option<fs::File> = None;
+    let mut write = false;
     for line in read_to_string(filename).unwrap().lines() {
         let s = line.to_string();
         // println!("{}",s);
 
         if s.contains("staticPath:") {
             if let Some(path_part) = s.split(':').nth(1) {
-                let path_part_trimmed = path_part.trim(); // Trims any extra whitespace
-                parsed_path = path_part_trimmed.to_string(); // Assign the string directly
+                let path_part_trimmed = path_part.trim();
 
-                if let Some(image_parsed_base) = parsed_path.split("/").nth(2) {
-                    image_base = image_parsed_base.to_string();
-                }
+                parsed_path = path_part_trimmed.to_string();
 
                 let mut static_base = hugo_base_dir.clone();
 
-                static_base.push_str("/static");
+                static_base.push_str("static/");
+                static_base.push_str(&parsed_path);
 
-                println!("static base {}", static_base);
+                println!("created static folder {}", static_base);
                 fs::create_dir_all(static_base.clone()).unwrap();
+
+                let mut hugo_md_file = hugo_base_dir.clone();
+                hugo_md_file.push_str("content/posts/");
+                hugo_md_file.push_str(&parsed_path);
+                hugo_md_file.push_str(".md");
+                println!("Creating hugo md file {}", hugo_md_file);
+                file = Some(File::create(hugo_md_file.clone()).unwrap());
+            }
+        }
+        if s.contains("---") {
+            if !file.is_none() {
+                write = true;
             }
         }
         if s.contains("![[") {
@@ -50,24 +62,43 @@ fn process_blog(
             attachment_base.push_str(imagename);
 
             let mut dest_path = hugo_base_dir.clone();
+            dest_path.push_str("static/");
             dest_path.push_str(&parsed_path);
-            dest_path.push_str(&image_base);
+            dest_path.push_str("/");
+            dest_path.push_str(&parsed_path);
             dest_path.push_str(&count.to_string());
             dest_path.push_str(".png");
-            
-            println!("copied {} to {}",attachment_base,dest_path);
+
+            println!("copied {} to {}", attachment_base, dest_path);
 
             fs::copy(attachment_base.clone(), dest_path.clone()).unwrap();
 
-           /*  println!(
-                "{}",
-                "![img](".to_owned() + &parsed_path + &image_base + &count.to_string() + ".png)"
-            ); */
+            let new_image_markdown: String = "![img](".to_owned()
+                + &parsed_path
+                + "/"
+                + &parsed_path
+                + &count.to_string()
+                + ".png)";
+
+            match file {
+                Some(ref file) =>write_to_file(file, new_image_markdown),
+                None => panic!("Trying to write image markdown to file without declaring `staticPath` have you set in the obsidian file?"),
+            }
+
             count += 1;
         } else {
-            //println!("{}", s);
+            if write && !s.contains("---") {
+                match file {
+                Some(ref file) =>write_to_file(file, s),
+                None => panic!("Trying to text to file without declaring `staticPath` have you set in the obsidian file?"),
+            }
+            }
         }
     }
+}
+
+fn write_to_file(mut file: &File, str: String) {
+    file.write((str  + "\n").as_bytes()).unwrap();
 }
 
 /// Progam to generate hugo markdown from obsidian markdown
@@ -75,15 +106,15 @@ fn process_blog(
 #[command(version, about, long_about = None)]
 struct Args {
     /// path to obsidian source markdown
-    #[arg( long)]
+    #[arg(long)]
     obsidian_dir: String,
 
     ///path to obsidian img folder
-    #[arg( long)]
+    #[arg(long)]
     obsidian_dir_imgs: String,
 
     /// path to hugo folder
-    #[arg( long)]
+    #[arg(long)]
     hugo_path: String,
 }
 
